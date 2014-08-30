709070947e4268000e13f8177a4220b9d7f16046
Tony McKeown <thetonic2001@gmail.com>
2014-08-31 00:00:11 +0100

    Created routes and amended profile controller so /profile_name shows their profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'profile/show'
+
   devise_for :users
   root 'pages#home'
   get "about" => "pages#about"
   get "whatsnew" => "pages#whatsnew"
+
+  get '/:id', to: 'profile#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
