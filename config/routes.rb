baf0256911b91ce86a0b18d3890281b1b8786dab
Tony McKeown <thetonic2001@gmail.com>
2014-08-31 04:15:53 +0100

    Renamed profile_controller to profiles_controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "about" => "pages#about"
   get "whatsnew" => "pages#whatsnew"
 
-  get '/:id', to: 'profile#show'
+  get '/:id', to: 'profiles#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
