3abaa519339bfc51d877030912d225804fb0d0d2
Tony McKeown <thetonic2001@gmail.com>
2014-08-31 16:55:14 +0100

    Added gender to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831032229) do
+ActiveRecord::Schema.define(version: 20140831155308) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140831032229) do
     t.string   "last_name"
     t.string   "profile_name"
     t.date     "date_of_birth"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
