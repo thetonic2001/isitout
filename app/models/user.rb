420eba06abec724b502de81adcd72357c484db7c
Tony McKeown <thetonic2001@gmail.com>
2014-08-31 04:37:26 +0100

    Created method to show age instead of date of birth on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,11 @@ class User < ActiveRecord::Base
 		profile_name
 	end
 
+	def age
+    now = Time.now.utc.to_date
+    now.year - date_of_birth.year - ((now.month > date_of_birth.month || (now.month == date_of_birth.month && now.day >= date_of_birth.day)) ? 0 : 1)
+end
+
 
 	def full_name
 		first_name + " " + last_name
